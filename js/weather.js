6c27ea24c47f9aa01d413dc68e4b509f4bcecdd5
Raul Camacho <rcamacho96@gmail.com>
2016-01-26 13:54:28 -0600

    added weather icons

---
+++ js/weather.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  var zip = "";
+  $('.btn').click(function(){
+    zip = document.getElementById('zip').value;
+
+  if (zip.length == 5) {
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us&APPID=58229a4e217926a79008cbfb61134884", function(data){
+      var temp = Math.round((data.main.temp - 273.15)* 1.8000 + 32.00);
+      var high = Math.round((data.main.temp_max - 273.15)* 1.8000 + 32.00);
+      var low = Math.round((data.main.temp_min - 273.15)* 1.8000 + 32.00);
+      $('.current').text(temp + '\u00B0' + 'F' + ', High: ' + high + ', Low: ' + low);
+    });
+  }
+  else {
+    alert("Uh oh! Please enter a valid 5-digit, U.S. zipcode.");
+  }
+  });
+});
